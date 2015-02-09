8d18a46f4502e8473f3c19373b753f2c84536b43
Misha Panyushkin <misha.panyushkin@gmail.com>
2015-02-10 01:59:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Creation:
+```
+var single = new Perceptron(n /*Integer*/);
+```
+Getting feed forward output:
+```
+var output = single.feedFroward(signals /*Array*/);
+```
+Train the perceptron:
+```
+single.train(signals /*Array*/, output /*Number*/)
+```
---
+++ src/Perceptron.js
@@ -0,0 +1,32 @@
+function Perceptron (n) {
+	this.weights = this.weights || [];
+	while (n--) {
+		this.weights[n] == Math.random() + Math.exp(-10);
+	}
+	this.bias = Math.random() + Math.exp(-10);
+	this.learning_constant = Math.random() + Math.exp(-2);
+}
+
+Perceptron.prototype = {
+
+	feedForward: function (signals) {
+		return this.activation(function (signals) {
+			return signals.reduce(function (input, i) {
+				return input*this.weights[i];
+			}.bind(this), 0) + this.bias;
+		}.call(this, signals));
+	},
+
+
+	activation: function (sum) {
+		return (Math.exp(sum) - Math.exp(-sum))/(Math.exp(sum) + Math.exp(-sum));
+	},
+
+
+	train: function (signals, output) {
+		this.weights.forEach(function (error, weight) {
+			weight += this.learning_constant*error*weight;
+		}.bind(this, output - this.feedForward(signals)));
+	}
+
+};
